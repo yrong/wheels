c6a0a862b2da4246be1b6628ce9b8508ef87cca0
yrong <yrong1997@gmail.com>
2017-11-02 23:25:52 +0800

    缓存通过searchByCypher接口查询db数据增加plain标识不做组类资源转换(0.5h)

--- scirichon-cache/index.js
+++ scirichon-cache/index.js
@@ -85,7 +85,7 @@ const loadOne = async (category,uuid)=>{
         }else if(_.isString(uuid)){
             name = uuid,body = {category,name,cypher:`MATCH (n:${category}) WHERE n.name={name} RETURN n`}
         }
-        item = await common.apiInvoker('POST',load_url.cmdb_url,'/searchByCypher',{'origional':true},body)
+        item = await common.apiInvoker('POST',load_url.cmdb_url,'/searchByCypher',{'origional':true,'plain':true},body)
         item = item.data||item
         await saveItem(item)
     }
